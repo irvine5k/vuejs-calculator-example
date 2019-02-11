68fd7e37be27167d0c65c20a6affa9e97560f136
Kelven Irvine <kelvengalvao@gmail.com>
2019-02-11 20:20:06 -0200

    Vue JS Calculator Example

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app">
+        <h1>Calculator</h1>
+        <Calculator/>
+    </div>
+</template>
+
+<script>
+
+import Calculator from "./main/Calculator"
+export default {
+    components: { Calculator }
+}
+</script>
+
+<style>
+    @font-face {
+        font-family: "RobotoMono";
+        src: url("./fonts/RobotoMono-Thin.ttf");
+    }
+
+    * {
+        font-family: "RobotoMono", "monospace";
+    }
+
+    body {
+        margin: 0;
+    }
+
+    #app {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: #fff;
+        background: linear-gradient(to right, rgb(178, 178, 253), purple);
+    }
+
+</style>
